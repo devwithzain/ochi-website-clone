68801c79f690306b72028361803ae9fbee31ff92
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-04-17 10:59:00 +0500

    completed

--- components/BackgroundImg.tsx
+++ components/BackgroundImg.tsx
@@ -2,14 +2,14 @@ import Image, { StaticImageData } from "next/image";
 
 export default function BackgroundImg({ src }: { src: StaticImageData }) {
 	return (
-		<div className="w-full relative overflow-hidden rounded-[20rem] bg-background">
+		<div className="w-full relative overflow-hidden rounded-[20px] bg-background">
 			<div
 				className="w-full"
 				data-scroll
 				data-scroll-speed="-.2.5">
 				<Image
 					src={src}
-					alt="asd"
+					alt="img"
 					className="w-full h-full object-cover"
 				/>
 			</div>
