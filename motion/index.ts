996f2e947d89ef8cb8dceeaa314aa5aeb8bece20
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-04-20 23:57:06 +0500

    modified

--- motion/index.ts
+++ motion/index.ts
@@ -115,3 +115,24 @@ export const translate = {
         transition: { duration: .75, ease: [0.76, 0, 0.24, 1] }
     }
 };
+
+
+export const opacity = {
+    initial: {
+        opacity: 0
+    },
+    enter: {
+        opacity: 0.75,
+        transition: { duration: 1, delay: 0.2 }
+    },
+};
+
+export const slideUp = {
+    initial: {
+        top: 0
+    },
+    exit: {
+        top: "-100vh",
+        transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.2 }
+    }
+};
