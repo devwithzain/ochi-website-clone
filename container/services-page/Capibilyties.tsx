57a674f5e10757d728b4db09d2c819418fb3dfc5
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-04-20 23:57:23 +0500

    modified

--- container/services-page/Capibilyties.tsx
+++ container/services-page/Capibilyties.tsx
@@ -12,26 +12,27 @@ export default function Capibilyties() {
 			<div className="w-[82%] sm:w-full xm:w-full padding-x mb-[70px]">
 				<h1 className="sub-heading font-medium font-NeueMontreal text-secondry">
 					<span className="link-flash cursor-pointer">Let’s be honest.</span>
-					There are really no excuses to have a bad presentation anymore. No one
-					has time for poorly communicated ideas. Focus on what you do best —
+					&nbsp;There are really no excuses to have a bad presentation anymore.
+					No one has time for poorly communicated ideas. Focus on what you do
+					best <span className="sm:hidden xm:hidden">—</span>
 					growing your business, while we do our best at &nbsp;
 					<span className="link-flash cursor-pointer">
 						making your presentations awesome.
 					</span>
 				</h1>
 			</div>
-			<div className="w-full flex justify-between py-[20px] padding-x border-t border-[#21212155]">
+			<div className="w-full flex sm:flex-col xm:flex-col justify-between py-[20px] padding-x border-t border-[#21212155] sm:gap-[15px] xm:gap-[15px]">
 				<div className="w-[10%] sm:w-full xm:w-full">
 					<h3 className="paragraph font-medium text-secondry font-NeueMontreal">
 						Our Capabilities:
 					</h3>
 				</div>
-				<div className="w-[90%] flex flex-col gap-y-[50px]">
+				<div className="w-[90%] flex flex-col gap-y-[30px]">
 					{serviceCapaybilitiesItem.map((item) => (
 						<div
-							className="w-full flex justify-between"
+							className="w-full flex sm:flex-col xm:flex-col justify-between sm:gap-[15px] xm:gap-[15px]"
 							key={item.id}>
-							<div className="w-[40%] flex justify-end mr-[150px]">
+							<div className="w-[40%] flex justify-end mr-[150px] sm:hidden xm:hidden">
 								{hovered && item.id === 1 ? (
 									<Image
 										src={item.src1}
@@ -53,8 +54,8 @@ export default function Capibilyties() {
 									)
 								)}
 							</div>
-							<div className="w-[25%]">
-								<div className="flex gap-x-[10px] flex-col pb-[20px]">
+							<div className="w-[25%] sm:w-full xm:w-full">
+								<div className="flex gap-x-[10px] flex-col">
 									<div className="flex items-center gap-x-[10px] pb-[20px]">
 										<span className="w-[12px] h-[12px] rounded-full bg-secondry" />
 										<h1 className="paragraph uppercase font-medium font-NeueMontreal text-secondry">
@@ -63,7 +64,7 @@ export default function Capibilyties() {
 									</div>
 								</div>
 								<div
-									className="flex flex-col gap-y-[12px]"
+									className="flex flex-col gap-y-[5px]"
 									onMouseEnter={() => setHovered(item.id === 1 && true)}
 									onMouseLeave={() => setHovered(item.id === 1 && false)}>
 									{item.links1.map((link) => (
@@ -75,8 +76,8 @@ export default function Capibilyties() {
 									))}
 								</div>
 							</div>
-							<div className="w-[25%]">
-								<div className="flex gap-x-[10px] flex-col pb-[20px]">
+							<div className="w-[25%] sm:w-full xm:w-full">
+								<div className="flex gap-x-[10px] flex-col">
 									<div className="flex items-center gap-x-[10px] pb-[20px]">
 										<span className="w-[12px] h-[12px] rounded-full bg-secondry" />
 										<h1 className="paragraph uppercase font-medium font-NeueMontreal text-secondry">
@@ -85,7 +86,7 @@ export default function Capibilyties() {
 									</div>
 								</div>
 								<div
-									className="flex flex-col gap-y-[12px]"
+									className="flex flex-col gap-y-[5px]"
 									onMouseEnter={() => setHovered1(item.id === 1 && true)}
 									onMouseLeave={() => setHovered1(item.id === 1 && false)}>
 									{item.links2.map((link) => (
