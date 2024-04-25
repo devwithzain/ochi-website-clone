4d64174dbf9a72b5bd57ecf21c8f4f2426efe3ee
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-04-25 23:59:46 +0500

    modified

--- container/workiz-page/Chelenge.tsx
+++ container/workiz-page/Chelenge.tsx
@@ -15,19 +15,19 @@ export default function Chelenge() {
 	return (
 		<section className="w-full pb-[100px]">
 			<div className="w-full flex flex-col">
-				<h1 className="text-[65px] padding-x leading-[65px] font-medium font-NeueMontreal text-secondry border-b pb-[50px] border-[#21212155]">
+				<h2 className="sub-heading padding-x font-medium font-NeueMontreal text-secondry border-b pb-[50px] border-[#21212155]">
 					Challenge & Solution
-				</h1>
+				</h2>
 				<div className="w-full border-t border-[#21212155] pt-[20px]">
-					<div className="w-full flex justify-between gap-[15px] px-[50px]">
+					<div className="w-full flex sm:flex-col xm:flex-col justify-between gap-[15px] padding-x">
 						<div>
-							<h3 className="text-[20px] leading-[30px] font-medium text-secondry font-NeueMontreal">
+							<h3 className="paragraph font-medium text-secondry font-NeueMontreal">
 								Services we provided:
 							</h3>
 						</div>
-						<div className="w-[50%] flex gap-[40px]">
+						<div className="w-[50%] flex gap-y-[20px] sm:w-full xm:w-full">
 							<div className="flex flex-col gap-y-[20px]">
-								<p className="text-[20px] leading-[30px] font-NeueMontreal text-secondry">
+								<p className="paragraph font-NeueMontreal text-secondry">
 									The goal was to build straightforward, <br /> professional,
 									but emotional presentations
 									<br /> that are delivered to colleagues and
@@ -46,15 +46,15 @@ export default function Chelenge() {
 			<div className="w-full padding-x py-[100px]">
 				<BackgroundImg src={chelengeBg} />
 			</div>
-			<div className="w-[80%] mx-auto py-[50px] flex gap-[20px]">
-				<div className="w-[49%]">
+			<div className="w-[80%] mx-auto padding-x py-[50px] flex gap-[20px] sm:w-full xm:w-full sm:flex-col xm:flex-col">
+				<div className="w-[49%] sm:w-full xm:w-full">
 					<Image
 						src={showcase1}
 						className="w-full  object-cover rounded-[20px]"
 						alt="chelengeBgImg"
 					/>
 				</div>
-				<div className="w-[49%]">
+				<div className="w-[49%] sm:w-full xm:w-full">
 					<Image
 						src={showcase2}
 						className="w-full  object-cover rounded-[20px]"
@@ -65,15 +65,15 @@ export default function Chelenge() {
 			<div className="w-full padding-x py-[100px]">
 				<BackgroundImg src={showcase3} />
 			</div>
-			<div className="w-[80%] mx-auto py-[50px] flex gap-[20px]">
-				<div className="w-[49%]">
+			<div className="w-[80%] mx-auto padding-x py-[50px] flex gap-[20px] sm:w-full xm:w-full sm:flex-col xm:flex-col">
+				<div className="w-[49%] sm:w-full xm:w-full">
 					<Image
 						src={showcase4}
 						className="w-full  object-cover rounded-[20px]"
 						alt="chelengeBgImg"
 					/>
 				</div>
-				<div className="w-[49%]">
+				<div className="w-[49%] sm:w-full xm:w-full">
 					<Image
 						src={showcase5}
 						className="w-full  object-cover rounded-[20px]"
@@ -85,11 +85,11 @@ export default function Chelenge() {
 				<BackgroundImg src={showcase6} />
 			</div>
 
-			<div className="w-[80%] mx-auto py-[50px] flex gap-[20px]">
+			<div className="w-[80%] sm:w-full xm:w-full mx-auto padding-x py-[50px] flex gap-[20px]">
 				<div className="w-full">
 					<Image
 						src={after}
-						className="w-full  object-cover rounded-[20px]"
+						className="w-full object-cover rounded-[20px]"
 						alt="chelengeBgImg"
 					/>
 				</div>
