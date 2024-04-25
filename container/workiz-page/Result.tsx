8880c642244f39f61d465b8d0b58f34176fa2fce
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-04-25 23:59:52 +0500

    modified

--- container/workiz-page/Result.tsx
+++ container/workiz-page/Result.tsx
@@ -14,22 +14,22 @@ export default function Result() {
 	return (
 		<section className="w-full py-[100px]">
 			<div className="w-full flex flex-col">
-				<h1 className="text-[65px] padding-x leading-[65px] font-medium font-NeueMontreal text-secondry pb-[50px]">
+				<h1 className="sub-heading padding-x font-medium font-NeueMontreal text-secondry pb-[50px]">
 					The Result
 				</h1>
 				<div className="w-full border-t border-[#21212155] pt-[20px]">
-					<div className="w-full flex justify-between gap-[15px] px-[50px]">
+					<div className="w-full flex sm:flex-col xm:flex-col justify-between gap-y-[20px] padding-x">
 						<div>
-							<h3 className="text-[20px] leading-[30px] font-medium text-secondry font-NeueMontreal">
+							<h3 className="paragraph font-medium text-secondry font-NeueMontreal">
 								Project Outcomes:
 							</h3>
 						</div>
-						<div className="w-[50%] flex gap-[40px]">
-							<div className="flex flex-col gap-y-[20px]">
-								<p className="text-[20px] leading-[30px] font-NeueMontreal text-secondry underline">
+						<div className="w-[50%] flex sm:flex-col xm:flex-col gap-[20px]">
+							<div className="flex flex-col gap-y-[20px] sm:w-full xm:w-full">
+								<p className="paragraph font-NeueMontreal text-secondry underline">
 									Feedback
 								</p>
-								<p className="text-[20px] leading-[30px] font-NeueMontreal text-secondry">
+								<p className="paragraph font-NeueMontreal text-secondry">
 									“OCHI brought certain level of <br /> professionalism into our
 									presentations that
 									<br /> we were lacking before. When I showed <br />
@@ -41,11 +41,11 @@ export default function Result() {
 									clients.”
 								</p>
 							</div>
-							<div className="flex flex-col gap-y-[20px]">
-								<p className="text-[20px] leading-[30px] font-NeueMontreal text-secondry underline">
+							<div className="flex flex-col gap-y-[20px] sm:w-full xm:w-full">
+								<p className="paragraph font-NeueMontreal text-secondry underline">
 									The Result
 								</p>
-								<p className="text-[20px] leading-[30px] font-NeueMontreal text-secondry">
+								<p className="paragraph font-NeueMontreal text-secondry">
 									We’ve created 10 presentations for Workiz <br /> company which
 									improved the on-boarding <br /> process, empowered sales team,
 									and just made <br />
