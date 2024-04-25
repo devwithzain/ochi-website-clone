48a577e0dee0b4a8da1b0819154fb6c2eeed4abf
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-04-26 00:00:11 +0500

    modified

--- container/workiz-page/Works.tsx
+++ container/workiz-page/Works.tsx
@@ -6,14 +6,16 @@ import { Marquee, ProjectCard, Rounded, Tags } from "@/components";
 export default function Works() {
 	return (
 		<section className="w-full bg-marquee rounded-t-[20px] sticky -top-[95%]">
-			<Marquee
-				title="other projects"
-				className="text-[516px] leading-[310px] pb-[55px]"
-			/>
-			<div className="w-full flex justify-between gap-y-[50px] padding-x pb-[50px] flex-wrap">
+			<div className="w-full bg-marquee z-10 relative rounded-t-[20px] pt-[80px]">
+				<Marquee
+					title="other projects"
+					className="pb-[50px] lg:pb-[40px] md:pb-[30px] sm:pb-[20px] xm:pb-[15px] text-[540px] leading-[330px] lg:text-[380px] lg:leading-[240px] md:text-[300px] md:leading-[160px] sm:text-[230px] sm:leading-[140px] xm:text-[130px] xm:leading-[80px] "
+				/>
+			</div>
+			<div className="w-full flex justify-between gap-y-[50px] padding-x pb-[50px] pt-[20px] flex-wrap">
 				{workizItem.map((item) => (
 					<div
-						className="w-[49%] sm:w-full"
+						className="w-[49%] sm:w-full xm:w-full"
 						key={item.id}>
 						<div className="flex gap-x-[10px] items-center pb-[10px]">
 							<span className="w-[10px] h-[10px] rounded-full bg-white" />
@@ -38,21 +40,22 @@ export default function Works() {
 					</div>
 				))}
 			</div>
-			<div className="w-full flex pt-[50px] pb-[200px] justify-center">
+			<div className="w-full flex pt-[30px] pb-[200px] justify-center">
 				<div className="flex items-center justify-between bg-white cursor-pointer rounded-full group">
 					<Link
-						className="text-[19px] text-secondry uppercase font-normal font-NeueMontreal"
+						className="paragraph text-secondry uppercase font-normal font-NeueMontreal"
 						href={"/presentation"}>
 						<Rounded
-							className="py-[10px]"
+							className="py-[6px]"
 							backgroundColor="#212121">
-							<p className="text-secondry z-10 px-[15px] ml-[30px] py-[10px] group-hover:text-white">
+							<p className="text-secondry z-10 px-[10px] ml-[15px] py-[6px] group-hover:text-white">
 								view all case studies
 							</p>
-							<div className="bg-secondry p-[15px] rounded-full scale-[0.3] mr-[17px] group-hover:scale-[1] transition-all z-10 text-secondry group-hover:bg-white  duration-300 ease-in-out">
+							<div className="bg-secondry p-[10px] rounded-full scale-[0.3] mr-[10px] group-hover:scale-[0.9] transition-all z-10 text-secondry group-hover:bg-white  duration-300 ease-in-out">
 								<ArrowUpRight
 									strokeWidth={1.5}
-									size={25}
+									size={30}
+									className="scale-[0] group-hover:scale-[1]"
 								/>
 							</div>
 						</Rounded>
