bd99c0a0b22c63626a027deeec797d31bc8f20bc
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-04-25 23:58:30 +0500

    modified

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,6 +6,7 @@ import { navVariants } from "@/motion";
 import { TextHover } from "@/animation";
 import { navbarItems } from "@/constants";
 import { useMotionValueEvent, useScroll, motion } from "framer-motion";
+import MobileNav from "./MobileNav";
 
 export default function Navbar() {
 	const [hidden, setHidden] = useState(false);
@@ -21,35 +22,38 @@ export default function Navbar() {
 	});
 
 	return (
-		<motion.nav
-			variants={navVariants}
-			className="w-full h-[8vh] padding-x fixed top-0 left-0 z-50 backdrop-blur-[7px] flex items-center justify-between"
-			animate={hidden ? "hidden" : "vissible"}>
-			<div className="w-[50%]">
-				<Link href={"/"}>
-					<Image
-						src={logo}
-						alt="ochi logo"
-						width={70}
-						height={70}
-					/>
-				</Link>
-			</div>
-			<div className="flex gap-x-[20px] sm:hidden xm:hidden w-[50%]">
-				{navbarItems.map((item) => (
-					<Link
-						key={item.id}
-						className={`w-fit text-[21px] leading-[32px] lg:text-[16px] lg:leading-[24px] md:text-[16px] md:leading-[24px] font-medium font-NeueMontreal text-secondry capitalize flex flex-col hover ${
-							item.id === 5 && "ml-[220px]"
-						}`}
-						href={item.href}>
-						<TextHover
-							titile1={item.title}
-							titile2={item.title}
+		<>
+			<motion.nav
+				variants={navVariants}
+				className="w-full h-[8vh] padding-x fixed top-0 left-0 z-50 backdrop-blur-[7px] flex items-center justify-between sm:hidden xm:hidden md:hidden"
+				animate={hidden ? "hidden" : "vissible"}>
+				<div className="w-[50%]">
+					<Link href={"/"}>
+						<Image
+							src={logo}
+							alt="ochi logo"
+							width={70}
+							height={70}
 						/>
 					</Link>
-				))}
-			</div>
-		</motion.nav>
+				</div>
+				<div className="flex gap-x-[20px] w-[50%]">
+					{navbarItems.map((item) => (
+						<Link
+							key={item.id}
+							className={`w-fit text-[21px] leading-[32px] lg:text-[16px] lg:leading-[24px] md:text-[16px] md:leading-[24px] font-medium font-NeueMontreal text-secondry capitalize flex flex-col hover ${
+								item.id === 5 && "ml-auto"
+							}`}
+							href={item.href}>
+							<TextHover
+								titile1={item.title}
+								titile2={item.title}
+							/>
+						</Link>
+					))}
+				</div>
+			</motion.nav>
+			<MobileNav />
+		</>
 	);
 }
