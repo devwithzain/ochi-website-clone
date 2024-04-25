903c56b2391bfd364229a07ebbbad0614c005f89
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-04-25 23:58:45 +0500

    modified

--- container/workiz-page/About.tsx
+++ container/workiz-page/About.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { welcome } from "@/public";
 import { BackgroundImg } from "@/components";
 
@@ -6,39 +5,39 @@ export default function About() {
 	return (
 		<section className="w-full py-[100px]">
 			<div className="w-full flex flex-col">
-				<h1 className="text-[65px] padding-x leading-[65px] font-medium font-NeueMontreal text-secondry pb-[50px]">
+				<h2 className="sub-heading padding-x font-medium font-NeueMontreal text-secondry pb-[50px]">
 					Compony
-				</h1>
+				</h2>
 				<div className="w-full border-t border-[#21212155] pt-[20px]">
-					<div className="w-full flex justify-between gap-[15px] px-[50px]">
+					<div className="w-full flex sm:flex-col xm:flex-col justify-between gap-[15px] padding-x">
 						<div>
-							<h3 className="text-[20px] leading-[30px] font-medium text-secondry font-NeueMontreal">
+							<h3 className="paragraph font-medium text-secondry font-NeueMontreal">
 								About:
 							</h3>
 						</div>
-						<div className="flex gap-[40px]">
+						<div className="flex gap-y-[20px] sm:flex-col xm:flex-col">
 							<div className="flex flex-col gap-y-[20px]">
-								<p className="text-[20px] leading-[30px] font-NeueMontreal text-secondry">
+								<p className="paragraph font-NeueMontreal text-secondry">
 									Built by home service professionals on a <br /> mission to
 									make your “work easy,” Workiz <br />
 									empowers service businesses to get more <br /> jobs done, work
 									easier, and grow smarter.
 								</p>
 							</div>
-							<div className="flex flex-col gap-[40px]">
+							<div className="flex flex-col gap-y-[20px]">
 								<div>
-									<p className="text-[20px] leading-[30px] font-NeueMontreal text-secondry underline">
+									<p className="paragraph font-NeueMontreal text-secondry underline">
 										Industry:
 									</p>
-									<p className="text-[20px] leading-[30px] font-NeueMontreal text-secondry">
+									<p className="paragraph font-NeueMontreal text-secondry">
 										Service Management Software
 									</p>
 								</div>
 								<div>
-									<p className="text-[20px] leading-[30px] font-NeueMontreal text-secondry underline">
+									<p className="paragraph font-NeueMontreal text-secondry underline">
 										Company Size:
 									</p>
-									<p className="text-[20px] leading-[30px] font-NeueMontreal text-secondry">
+									<p className="paragraph font-NeueMontreal text-secondry">
 										100+ People
 									</p>
 								</div>
