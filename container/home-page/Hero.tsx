9f14254b869d11d45e6731220c8e63bf982f6606
devwithzain <zainsoftwear11@gmail.com>
2024-05-16 11:51:00 -0700

    modified

--- container/home-page/Hero.tsx
+++ container/home-page/Hero.tsx
@@ -33,7 +33,7 @@ export default function Hero() {
 											height={50}
 											src={ochiside}
 											alt="img"
-											className="w-auto h-auto lg:w-auto lg:h-auto md:w-[100px] md:h-[63px] sm:w-[74px] sm:h-[45px] xm:w-[64px] xm:h-[40px] object-cover xl:mt-[15px] mt-[10px] rounded-[10px]"
+											className="w-auto h-[95px] lg:w-auto lg:h-auto md:w-[100px] md:h-[63px] sm:w-[74px] sm:h-[45px] xm:w-[64px] xm:h-[40px] object-cover xl:mt-[15px] mt-[10px] rounded-[10px]"
 										/>
 									</motion.span>
 									<h1 className="heading tracking-[-1.3px] text-[#212121] font-semibold font-FoundersGrotesk uppercase">
@@ -66,7 +66,7 @@ export default function Hero() {
 										From the first picth to IPO
 									</p>
 								</div>
-								<div className="flex items-center gap-[10px] group">
+								<div className="flex items-center gap-[5px] group">
 									<div className="rounded-[50px] border border-[#21212199] group-hover:bg-secondry  py-[3px] px-[12px] cursor-pointer">
 										<Link
 											className="paragraph font-NeueMontreal text-secondry uppercase group-hover:text-background transition-all  transform duration-[0.3s] ease-[.215,.61,.355,1]"
@@ -74,10 +74,10 @@ export default function Hero() {
 											start the project
 										</Link>
 									</div>
-									<div className="w-[30px] flex items-center justify-center h-[30px] border border-[#21212199] rounded-full p-[1px] sm:p-[30px] xm:pb-[30px]  group-hover:bg-secondry transition-all transform duration-[0.3s] ease-[.215,.61,.355,1] cursor-pointer xm:hidden sm:hidden">
+									<div className="w-[33px] flex items-center justify-center h-[33px] border border-[#21212199] rounded-full p-[1px] sm:p-[30px] xm:pb-[30px]  group-hover:bg-secondry transition-all transform duration-[0.3s] ease-[.215,.61,.355,1] cursor-pointer xm:hidden sm:hidden">
 										<p className="font-normal text-secondry group-hover:text-background">
 											<ArrowUpRight
-												size={22}
+												size={24}
 												strokeWidth={1.25}
 											/>
 										</p>
