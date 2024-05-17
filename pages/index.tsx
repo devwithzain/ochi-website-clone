627a650ebb8ed0992638793018846737436f2b88
devwithzain <zainsoftwear11@gmail.com>
2024-05-17 09:47:39 -0700

    modified

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 		<>
 			<Curve backgroundColor={"#f1f1f1"}>
 				<Hero />
-				<div className="w-full bg-marquee z-10 relative rounded-t-[20px] py-[100px]">
+				<div className="w-full bg-marquee z-10 relative rounded-t-[20px] padding-y">
 					<Marquee
 						title="we are ochi"
 						className="pb-[50px] lg:pb-[40px] md:pb-[30px] sm:pb-[20px] xm:pb-[15px] text-[540px] leading-[330px] lg:text-[380px] lg:leading-[240px] md:text-[300px] md:leading-[160px] sm:text-[230px] sm:leading-[140px] xm:text-[130px] xm:leading-[80px]"
