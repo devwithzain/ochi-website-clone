76974628235913a17a09052d16d37f6ad31edcfa
devwithzain <zainsoftwear11@gmail.com>
2024-05-17 09:46:44 -0700

    modified

--- public/index.ts
+++ public/index.ts
@@ -1,5 +1,6 @@
 export { default as eyes } from './eyes.svg';
 export { default as logo } from './logo.svg';
+export { default as mobileLogo } from './mobile-logo.png';
 export { default as awwwards } from './awwwards.svg';
 export { default as aboutImg } from './aboutImg.jpeg';
 export { default as videoImg } from './videoImg.jpeg';
