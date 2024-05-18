d4484b2d565974a76e4f769219cac9a218408daf
devwithzain <zainsoftwear11@gmail.com>
2024-05-18 04:21:20 -0700

    modified

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
 	const phrase = ["Eye-", "opening"];
 	const phrase1 = ["PRESENTATIONS"];
 	return (
-		<footer className="w-full min-h-screen padding-x z-30 relative pt-[40px] bg-background flex flex-col justify-between">
+		<footer className="w-full min-h-screen padding-x z-30 relative pt-[40px] bg-background flex flex-col justify-between rounded-t-[20px] mt-[-20px]">
 			<div className="w-full flex justify-between sm:flex-col xm:flex-col">
 				<div className="flex flex-col justify-between sm:w-full xm:w-full w-1/2">
 					<h1 className="text-[150px] leading-[115px] lg:text-[130px] lg:leading-[98px] md:text-[100px] md:leading-[75px] sm:text-[74px] sm:leading-[68px] xm:text-[64px] xm:leading-[48px] font-semibold font-FoundersGrotesk text-secondry uppercase">
@@ -100,7 +100,7 @@ export default function Footer() {
 					</Link>
 				</div>
 				<div className="w-1/2 h-full flex gap-[10px] justify-between items-end sm:w-full xm:w-full sm:flex-col xm:flex-col sm:items-start xm:items-start">
-					<div className="flex items-center gap-x-[10px]">
+					<div className="flex sm:flex-col xm:flex-col gap-[10px]">
 						<h1 className="paragraph font-medium font-NeueMontreal text-secondry opacity-40">
 							© ochi design 2024.
 						</h1>
@@ -110,7 +110,7 @@ export default function Footer() {
 							className="before:h-[1px] after:h-[1px] paragraph font-medium text-secondry opacity-40 before:bottom-[-3px] after:bottom-[-3px]"
 						/>
 					</div>
-					<div className="">
+					<div>
 						<LinkHover
 							title="Website by Zain"
 							href="/"
